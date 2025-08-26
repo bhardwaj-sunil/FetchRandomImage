8c646b7ca2ad26ec0ea0c0050016fc041ecaff6d
bhardwaj-sunil <sbpersistent@gmail.com>
2025-08-25 23:32:15 -0400

    Implementing UI
    1. Added local storage
    2. Deleted previous view and added tableview embedded with navigation
    3. Added tableview methods
    4. Added delete and move functionality

--- FetchRandomImage/View/RandomItemsViewController.swift
+++ FetchRandomImage/View/RandomItemsViewController.swift
@@ -7,23 +7,61 @@
 
 import UIKit
 
-class RandomItemsViewController: UIViewController {
+class RandomItemsViewController: UITableViewController {
 
-    let viewModel = RandomItemsViewModel()
+    private let viewModel = RandomItemsViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         title  = "Random items"
+        addNavigations()
+        updateView()
+    }
+    
+    // MARK: TableView override methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.randomItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "nativeCell") ?? UITableViewCell(style: .subtitle, reuseIdentifier: "nativeCell")
         
-        AddRandomItem()
+        let item = viewModel.randomItems[indexPath.row]
         
+        cell.textLabel?.text = item.author
+        cell.detailTextLabel?.text = item.id
+        
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            viewModel.deleteRandomITem(at: indexPath.row)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        viewModel.move(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    //MARK: Private methods
+    
+    private func addNavigations() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addRandomItem))
+        
+        navigationItem.leftBarButtonItem = editButtonItem
+    }
+    
+    private func updateView() {
+        viewModel.onUpdate = { [weak self] in
+            self?.tableView.reloadData()
+        }
     }
     
     // Add Random Item
-    private func AddRandomItem() {
+    @objc private func addRandomItem() {
         Task { await viewModel.addRandom() }
     }
-
 }
 
